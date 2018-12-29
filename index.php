3cec73e7c8d5b800b2cd8eb752ed3c6d8ebc6a10
Unknown <opticalfalicy@gmail.com>
2018-12-29 12:52:41 -0600

    First test code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    function myFunc($banddude, $lyric){
+        echo "<p>Hi, my name is $banddude, The world is a $lyric.</p>";
+    }
+
+    myFunc('Billy', 'vampire');
+?>
+
+<h1><?php bloginfo('name')?></h1>
+<p><?php bloginfo('description')?></p>
